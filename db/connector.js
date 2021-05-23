0fdcbdb54bb0c2b80015abf1d743543c0a3d96da
ottavio.buonomo <ottavio.buonomo@etu.hesge.ch>
2021-05-23 12:14:08 +0200

    Update IP Address

--- db/connector.js
+++ db/connector.js
@@ -1,10 +1,28 @@
 const mariadb = require('mariadb');
 
+const { networkInterfaces } = require('os');
+
+const nets = networkInterfaces();
+const results = []
+
+function getIP() {
+    for (const name of Object.keys(nets)) {
+        for (const net of nets[name]) {
+            // Skip over non-IPv4 and internal (i.e. 127.0.0.1) addresses
+            if (net.family === 'IPv4' && !net.internal) {
+                if (net.address.includes('10.0.0.'))
+                    results.push(net.address);
+            }
+        }
+    }
+}
+
 async function getConnection() {
+    getIP()
     let connection
     try {
         connection = mariadb.createConnection({
-            host: '192.168.1.9',
+            host: results[0],
             port: '3307',
             user: 'root',
             password: 'Datemianche1000lire_1',
